2bf62c0d91e72d3bc6ac98c3baf037debc4c35be
Al Zadid Sultan Bin Habib <ah00069@mix.wvu.edu>
2026-09-02 05:00:25 -0400

    Add files via upload

---
+++ augtab/__init__.py
@@ -0,0 +1,25 @@
+from .augtab import (
+    FALConfig,
+    BackboneConfig,
+    RegularizerConfig,
+    AugTabConfig,
+    AugTabCore,
+    AugTabClassifier,
+    AugTabMulti,
+    AugTabRegressor,
+)
+
+
+__all__ = [
+    "FALConfig",
+    "BackboneConfig",
+    "RegularizerConfig",
+    "AugTabConfig",
+    "AugTabCore",
+    "AugTabClassifier",
+    "AugTabMulti",
+    "AugTabRegressor",
+]
+
+
+__version__ = "0.1.0"
